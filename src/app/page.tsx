e947936a98d42a93fa8f2cc91fcbbd914d9240c0
Li7252 <lidiya.yasyrckina@gmail.com>
2023-05-21 12:46:20 +0300

    dynamic component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+'use client'
+
+import dynamic from 'next/dynamic';
+import React, { useState } from 'react'
+
+const Modal = dynamic(() => import('../components/DynamicImport'));
 
 const Home = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className='container mx-auto px-4'>Home</div>
+    <div className='container mx-auto py-5'>
+      <h1 className='text-4xl mt-5 border-b-2 border-slate-200'>Homework code-splitting</h1>
+      <div className='py-3'>
+        <button
+          className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+          onClick={() => setShowModal(true)}
+        >
+          Open modal
+        </button>
+      </div>
+      {showModal && <Modal setShowModal={setShowModal}  />}
+    </div>
   )
 }
 
